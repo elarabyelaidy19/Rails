7473cf274a888eb4a7530f0b71bfa7e4592a99ed
elaraby elaidy <elarabyelaidy@gmail.com>
2021-08-31 11:14:34 +0200

    books libirary

--- lib/lib/app/controllers/books_controller.rb
+++ lib/lib/app/controllers/books_controller.rb
@@ -1,44 +1,55 @@
 class BooksController < ApplicationController
 
-  def index 
-    books = Book.all 
+  def index
+    @books = Book.all
     render :index
   end
 
-  def show 
-    @book = Book.find_by(id: params[:id]) 
+  def show
+    @book = Book.find_by(id: params[:id])
 
-    if @book 
-      render :show 
-    else 
-      redirect_to books_url 
-    end  
+    if @book
+      render :show
+    else
+      redirect_to books_new_url
+    end
   end
 
-  def new 
-    @book = Book.new 
-    render :new 
+  def new
+    @book = Book.new
+    render :new
   end
 
-  def create 
-    @book = Book.new(book_params) 
+  def create
+    @book = Book.new(book_params)
 
-    if @book.save 
-      redirect_to book_url(@book) 
-    else 
-      render :new 
-    end 
-  end
-
-  def update
+    if @book.save
+      # show user the book show page
+      redirect_to book_url(@book)
+    else
+      # show user the new book form
+      render :new
+    end
   end
 
   def edit
+    @book = Book.find_by(id: params[:id])
+    render :edit
   end
 
-  private 
+  def update
+    @book = Book.find_by(id: params[:id])
 
-  def book_params 
+    if @book.update_attributes(book_params)
+      redirect_to book_url(@book)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def book_params
     params.require(:book).permit(:title, :author, :year, :description, :category)
-  end 
+  end
+
 end
--- lib/lib/app/models/book.rb
+++ lib/lib/app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord  
   validates :title, :author, presence: true
-
+  
+  def year_not_in_future
+    errors[:year] << "cannot be in the future" unless year < 2016
+  end
 end 
