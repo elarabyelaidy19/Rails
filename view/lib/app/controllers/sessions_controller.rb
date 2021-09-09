5840bd4ccfd77153338453dae7491f1dd5751988
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-09 19:33:48 +0200

    adding flash

--- view/lib/app/controllers/sessions_controller.rb
+++ view/lib/app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController 
+  
+  # login create session 
   def create 
     username = params[:user_name] 
     password = params[:password] 
@@ -14,5 +16,32 @@ class SessionsController < ApplicationController
 
     session[:session_token] = user.session_token 
     redirect_to feed_path 
+  end 
+
+  # log out destroy session set it to nil 
+  def destroy 
+    session_token = session[:session_token] 
+    user = User.find_by(session_token: session_token) 
+    
+    user.session_token = SecureRandom.urlsafe_base64(16) 
+    user.save!
+    session[:session_token] = nil 
+
+    # flash for the current request only seprated from next state and render the same page again
+    # using with rendering within the same request values stored for current request only
+    if user.nil? 
+      flash.now[:notices] =|| [] 
+      flash.now[:notices] << "faild login"  
+
+      render :new
+      return 
+    end 
+    
+    # creating flash for the current and next request 
+    # redirct and render using with
+    flash[:notices] =|| [] 
+    flash[:notices] << "You logged out! see you soon" 
+
+    redirect_to root_path 
   end 
 end
