bfc0e1b09c93c67aed9d1ce90c46fd660a74ee7e
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-12 16:52:27 +0200

    simple auth model

--- auth/railsauth/app/models/user.rb
+++ auth/railsauth/app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ApplicationRecord
+class User < ApplicationRecord 
+  
+  attr_reader :password 
+
+  before_validation :ensure_session_token 
+  validates :username, :session_token, presence: true  
+  validates :password_digest, presence: { message: 'password can not be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true } 
+
+  def password=(password) 
+    @password = password 
+  end 
 end
