165399164fbc1b0e2c6c0107c89ab917685c70c0
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-12 17:01:35 +0200

    simple auth model

--- auth/railsauth/app/models/user.rb
+++ auth/railsauth/app/models/user.rb
@@ -5,7 +5,27 @@ class User < ApplicationRecord
   before_validation :ensure_session_token 
   validates :username, :session_token, presence: true  
   validates :password_digest, presence: { message: 'password can not be blank' }
-  validates :password, length: { minimum: 6, allow_nil: true } 
+  validates :password, length: { minimum: 6, allow_nil: true }  
+
+  def find_by_credentials(username, password) 
+    user = User.find_by(username: username) 
+    return nil if user.nil? 
+    user.is_password?(password) ? user : nil 
+  end 
+
+  def self.generate_session_token 
+    SecureRandom::urlsafe_base64(16) 
+  end 
+
+  def reset_session_token 
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token  
+  end 
+
+  def ensure_session_token 
+    self.session_token = User.generate_session_token if self.session_token.nil? 
+  end 
 
   def password=(password) 
     @password = password 
