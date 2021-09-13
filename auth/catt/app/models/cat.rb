264f5dd9defcc4a4eb2f65849f7d7a7d76fe7510
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-14 01:32:15 +0200

    catt

---
+++ auth/catt/app/models/cat.rb
@@ -0,0 +1,26 @@
+require 'action_view' 
+class Cat < ApplicationRecord 
+  include ActionView::Helpers::DateHelper  
+
+  CAT_COLORS = %w(Black Orange Yellwo White ).freeze 
+  GENDERS = %w(M F).freeze
+
+  validates :birth_date, :color, :name, :sex, :description, presence: true  
+  validates :color, inclusion: CAT_COLORS,  unless: -> { color.blanck? } 
+  validates :sex, inclusion: GENDERS, if: -> { sex } 
+  validates :birth_date_in_the_past, if: -> { birth_date }
+
+  belongs_to :owner, 
+    class_name: 'User', 
+    foreign_key: :user_id
+
+  def age 
+    time_ago_in_words(birth_date) 
+  end  
+
+  private 
+
+  def birth_date_in_the_past 
+    if birth_date && birth_date > Time.now 
+      errors[:birth_date] << ' must be in the bast'
+end
