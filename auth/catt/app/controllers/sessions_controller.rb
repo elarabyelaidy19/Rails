115c6b0846cdc48c21ff02af1935b2a9cf4f96f0
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-14 02:50:18 +0200

    add user model

---
+++ auth/catt/app/controllers/sessions_controller.rb
@@ -0,0 +1,4 @@
+class SessionsController < ApplicationController
+  def Users
+  end
+end
--- auth/catt/config/routes.rb
+++ auth/catt/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'sessions/Users'
   # get 'cats/new'
   # get 'cats/index'
   # get 'cats/show'
