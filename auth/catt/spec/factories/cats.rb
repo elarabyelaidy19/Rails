b3ccdda7ed2f839655ebd90771413401805d67df
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-16 03:11:27 +0200

    add some tests

--- auth/catt/spec/factories/cats.rb
+++ auth/catt/spec/factories/cats.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :cat do
-    birth_date { "2021-09-14" }
-    color { "MyString" }
-    sex { "MyString" }
-    name { "MyString" }
-    description { "MyText" }
+    
+    color { Faker::Color.hex_color }
+    name { Faker::Name.name } 
+
+    
+
   end
 end
--- auth/catt/spec/models/cat_spec.rb
+++ auth/catt/spec/models/cat_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Cat, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  subject(:cat) { FactoryBot.build(:cat) }
+  describe "Validations" do 
+    it { should validate_presence_of(:name) } 
+
+    it "should validate presence of sex" do 
+      cat = Cat.new(sex: nil) 
+      expect(cat.valid?).to be false 
+    end  
+
+    it "should validate color" do 
+      cat = Cat.new(color: 'yellow') 
+      expect(cat.valid?).to be false 
+    end
+
+    it "should validate color is white" do 
+      white_cat = FactoryBot.build(:cat, color: "white") 
+      white_cat.valid? 
+      expect(white_cat).to be true 
+    end  
+
+
+  end  
+
+  describe "associations" do 
+    it { should have_many(:rental_requests) } 
+  end 
 end
