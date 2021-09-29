688ad9dbb2e69ef0233d0590c4a8a7b3eda1b82d
elaraby elaidy <elarabyelaidy@gmail.com>
2021-09-29 16:00:20 +0200

    Crud http server

--- rails_lite/http_server.rb
+++ rails_lite/http_server.rb
@@ -23,20 +23,55 @@ def handle_request(socket)
 
     if [verb, path] == ["GET", "/cats"] 
       socket.puts $cats.to_json 
-
+    # READ
     elsif verb == "GET" && cat_reg =~ path 
       match_date2 =  cat_reg.match(path) 
       cate_id = Integer(match_date2[1]) 
       cat = $cats.find { |cat| cat[:id] == cate_id} 
       socket.puts cat.to_json
       
+    # DELETE 
     elsif verb == "DELETE" && cat_reg =~ path 
       match_date2 =  cat_reg.match(path) 
       cate_id = Integer(match_date2[1]) 
       $cats.reject! {|cat| cat[:id] == cate_id }  
 
-      socket.puts true.to_json
+      socket.puts true.to_json 
+      
+    # CREATE 
+    elsif [verb, path] == ["POST", "/cats"] 
+      header1 = socket.gets.chomp 
+      match_date2 = /Content-length: (\d+)/.match(header1) 
+      content_length = Integer(match_date2[1]) 
+      socket.gets.chomp 
+
+      body_date = socket.gets.chomp 
+      cat = JSON.parse(body_date) 
+      cat[:id] = ($id += 1) 
+      $cats << cat  
+      socket.puts cat.to_json
+
+    # UPDATE
+    elsif verb == "PATCH" && cat_reg =~ path 
+      header1 = socket.gets.chomp 
+      match_date2 = /Content-length: (\d+)/.match(header1) 
+      content_length = Integer(match_date2[1]) 
+      socket.gets.chomp 
+
+      body_date = socket.gets.chomp 
+      parsed_body_data = JSON.parse(body_date)
+
+      match_date2 = cat_reg.match(path) 
+      cate_id = Integer(match_date2[1]) 
+
+      cat = $cats.find { |cat| cat[:id] = cate_id }
+      parsed_body_data.each do |k, v| 
+        cat[k] = v 
+      end 
+
+      socket.puts cat.to_json 
     end 
+ 
     socket.close
   end 
   puts "spawned worker thread"
